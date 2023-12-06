83a7d8e0267e581cce2e47a97070419cb48abde4
Chad Freeman <ceo@marinadev.xyz>
2023-12-05 19:29:39 -0500

    blah

---
+++ src/plr.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+var interface : XRInterface
+func _ready() -> void:
+	interface = XRServer.find_interface("OpenXR")
+	if interface and interface.is_initialized():
+		print("yipee")
+		get_viewport().use_xr = true
